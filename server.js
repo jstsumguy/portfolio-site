b3ab23e8c9d3d097bae156b1ec59c440c1c5e619
Willy <wcr5048@psu.edu>
2015-07-03 19:42:15 -0400

    added procfile

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use("/templates", express.static(__dirname + '/templates'));
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/layouts/index.html');
 })
-
-app.listen(3000, function () {
-	console.log('Server listening on port', 3000)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
